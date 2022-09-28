538feb35f4e8ef67010630ac26dae3125dfef3e7
Michael Bond <codemonkey8510@icloud.com>
2022-09-28 19:31:31 -0400

    Added some stuff

---
+++ MauiReactorBlazorHybrid/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiReactorBlazorHybrid;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MauiReactorStuff/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiReactorStuff;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
